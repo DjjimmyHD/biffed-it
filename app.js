12f9c2651cbb17be9ec2a1ac6810e03de6e45b91
James Schultz <james.schultz@A1-10110010.local>
2018-09-18 10:10:39 -0600

    inital commit added JS funtion to create heading in divs does not work 👨🏻

---
+++ app.js
@@ -0,0 +1,6 @@
+const placeHolder = document.querySelector('div')
+const heading = document.createElement('h1')
+heading.innerText = 'HIIIIIII'
+placeHolder.appendchild(heading)
+
+console.dir(placeHolder)
